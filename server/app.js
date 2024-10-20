5c76f7757d297c45d98c8580abf55d273dee9f0a
Putra Mahardika <putram.works@gmail.com>
2024-10-20 20:20:16 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
+// Load environment variables in non-production environments
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
 const port = process.env.PORT || 3000;
-// require("dotenv").config();
 const express = require("express");
 const routes = require("./routes/index");
 const errorHandler = require("./middleware/errorhandler");
 const corsOptions = require("./middleware/cors");
-const app = express();
 const cors = require("cors");
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const app = express();
+
+// Middleware
+app.use(cors(corsOptions)); // CORS options
+app.use(express.json()); // JSON body parser
+app.use(express.urlencoded({ extended: true })); // URL-encoded body parser
+app.use(express.static("public")); // Serve static files
+
+// Routes
 app.use(routes);
+
+// Centralized error handling
 app.use(errorHandler);
 
-app.listen(port, () => console.log("Running on port " + port));
+// Start the server
+app.listen(port, () => console.log(`Running on port ${port}`));
 
 module.exports = app;
