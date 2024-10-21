9b280a50523c99fb3626522439e8d3e8604310ad
Putra Mahardika <putram.works@gmail.com>
2024-10-21 21:07:03 +0700

    Update articleRoutes.js

--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -4,27 +4,32 @@ const ArticleController = require("../controllers/ArticleController");
 const authorizationArticle = require("../middleware/authorizationArticle");
 const upload = require("../middleware/multer");
 
+// Routes for Article management
 articleRouter.get("/get", ArticleController.getAllArticles);
 articleRouter.post("/add", ArticleController.createArticle);
+
 articleRouter.get(
-  "/:id",
-  authorizationArticle,
+  "/:id", 
+  authorizationArticle, 
   ArticleController.getArticleById
 );
+
 articleRouter.put(
-  "/:id",
-  authorizationArticle,
+  "/:id", 
+  authorizationArticle, 
   ArticleController.updateArticle
 );
+
 articleRouter.patch(
-  "/:id/image",
-  upload.single("img"), 
+  "/:id/image", 
   authorizationArticle, 
+  upload.single("img"), 
   ArticleController.updateArticleImage
 );
+
 articleRouter.delete(
-  "/:id",
-  authorizationArticle,
+  "/:id", 
+  authorizationArticle, 
   ArticleController.deleteArticle
 );
 
