780d036af17fb27688194e6a99eec5d941b5d8a1
Putra Mahardika <putram.works@gmail.com>
2024-10-21 21:06:09 +0700

    Update jsonwebtoken.js

--- server/helpers/jsonwebtoken.js
+++ server/helpers/jsonwebtoken.js
@@ -1,21 +1,17 @@
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.JWT;
 
-const generateToken = (payload) => {
-  return jwt.sign(payload, secretKey);
-};
+const generateToken = (payload) => jwt.sign(payload, secretKey);
 
 const verifyToken = (token) => {
   try {
     return jwt.verify(token, secretKey);
   } catch (error) {
-    throw error;
+    throw new Error("Invalid or expired token");
   }
 };
 
-const decodeToken = (token) => {
-  return jwt.decode(token);
-};
+const decodeToken = (token) => jwt.decode(token);
 
 module.exports = {
   generateToken,
