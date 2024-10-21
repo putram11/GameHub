0e2581df586a1a06fea9da81ad79a105d41b48f2
Putra Mahardika <putram.works@gmail.com>
2024-10-22 00:31:22 +0700

    Update CategoryController.js

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -1,22 +1,22 @@
 const { Category } = require("../models");
 
 class CategoryController {
+  // Create a new category
   static async createCategory(req, res, next) {
     const { name } = req.body;
 
     try {
-      const category = await Category.create({ name });
-      res.status(201).json(category);
+      const newCategory = await Category.create({ name });
+      res.status(201).json(newCategory);
     } catch (error) {
       if (error.name === "SequelizeValidationError") {
-        error.name = "ValidationError";
-        next(error);
-      } else {
-        next(error);
+        error.name = "ValidationError"; // Renaming for consistency
       }
+      next(error);
     }
   }
 
+  // Retrieve all categories
   static async getAllCategories(req, res, next) {
     try {
       const categories = await Category.findAll();
@@ -26,6 +26,7 @@ class CategoryController {
     }
   }
 
+  // Update a category by ID
   static async updateCategory(req, res, next) {
     const { id } = req.params;
     const { name } = req.body;
@@ -33,27 +34,30 @@ class CategoryController {
     try {
       const category = await Category.findByPk(id);
 
-      if (!category) throw { name: "NotFoundError" };
+      if (!category) {
+        throw { name: "NotFoundError" };
+      }
 
       await category.update({ name });
       res.status(200).json(category);
     } catch (error) {
       if (error.name === "SequelizeValidationError") {
         error.name = "ValidationError";
-        next(error);
-      } else {
-        next(error);
       }
+      next(error);
     }
   }
 
+  // Delete a category by ID
   static async deleteCategory(req, res, next) {
     const { id } = req.params;
 
     try {
       const category = await Category.findByPk(id);
 
-      if (!category) throw { name: "NotFoundError" };
+      if (!category) {
+        throw { name: "NotFoundError" };
+      }
 
       await category.destroy();
       res.status(200).json({ message: "Category successfully deleted" });
